a93b8bd24ce9f54bb8e20747c9da6214d641fff5
francismaile <francismaile@gmail.com>
2018-03-13 06:46:30 -0700

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,38 +1,43 @@
 const matrix = [];
 matrix[0] = [1,2,3,4];
-matrix[1] = [12,13,14,5];
-matrix[2] = [11,16,15,6];
-matrix[3] = [10,9,8,7];
+matrix[1] = [5,6,7,8];
+matrix[2] = [9,10,11,12];
+matrix[3] = [13,14,15,16];
 
-let result = [[],[],[],[]];
+function printSpiral( arr ) {
+  let result = [];
 
-let x = 0;
-let y = 0;
-let yinc = 0;
-let xinc = 1;
+  let x = 0;
+  let y = 0;
+  let yinc = 0;
+  let xinc = 1;
 
-const limit = 3;
-let goal = (limit + 1) * (limit + 1);
+  const limit = 3;
+  let goal = (limit + 1) * (limit + 1);
 
-while ( goal-- ) {
-  result[y][x] = matrix[y][x];
+  while ( goal-- ) {
+    result.push(arr[y][x]);
 
-	const yla = y + yinc;
-	const xla = x + xinc;
-	if( ( yinc !== 0 && (yla > limit || yla < 0) ) ||
-			( xinc !== 0 && (xla > limit || xla < 0) ) ||
-				result[yla][xla] !== undefined ) {
-		if( xinc !== 0 ) {
-			yinc = xinc;
-			xinc = 0;
-		} else if (yinc !== 0) {
-			xinc = -(yinc);
-			yinc = 0;
-		}
-	}
-	
-	x = x + xinc;
-	y = y + yinc;
+  	const yla = y + yinc;
+  	const xla = x + xinc;
+  	if( ( yinc !== 0 && (yla > limit || yla < 0) ) ||
+  			( xinc !== 0 && (xla > limit || xla < 0) ) ||
+  				result.includes(arr[yla][xla]) ) {
+  		if( xinc !== 0 ) {
+  			yinc = xinc;
+  			xinc = 0;
+  		} else if (yinc !== 0) {
+  			xinc = -(yinc);
+  			yinc = 0;
+  		}
+  	}
 
-}
+  	x = x + xinc;
+  	y = y + yinc;
+  } // end while
+  return result.join(' ');
+} // end printSpiral
+
+console.log( printSpiral(matrix) );
+
 
